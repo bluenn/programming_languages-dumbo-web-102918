d4ac740a4cc40bf01a536504d5611576088da0fe
a lan <bluenowte97@gmail.com>
2018-11-12 19:55:06 +0000

    automatically backed up by learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -7,6 +7,7 @@ intermediate = languages.map do |key, value|
    value.each do |name, info|
       reformatted[name] = info
       reformatted[name][:style] = []
+      reformatted[name][:style].push(languages[key])
      #value[:style].push(intermediate[key])
   end
 end
