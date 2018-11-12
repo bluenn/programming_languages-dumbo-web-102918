d12b6540ce496ab36f861fd12371fc3e7f1cc9a1
a lan <bluenowte97@gmail.com>
2018-11-12 20:02:37 +0000

    automatically backed up by learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,14 +1,11 @@
 def reformat_languages(languages)
-  # your code here
-
   reformatted = {}
-
-intermediate = languages.map do |key, value|
-   value.each do |name, info|
-      reformatted[name] = info
-      reformatted[name][:style] = []
-      array = reformatted[name][:style]
-      array.push(key)
+  intermediate = languages.map do |key, value|
+    value.each do |name, info|
+    reformatted[name] = info
+    reformatted[name][:style] = []
+    array = reformatted[name][:style]
+    array.push(key)
      #value[:style].push(intermediate[key])
   end
 end
